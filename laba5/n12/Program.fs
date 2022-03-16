bbe874e209888f6405c4eff384f88d501db655c9
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-17 00:09:30 +0300

    12 номер

---
+++ laba5/n12/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let lower (x: String) = x.ToLower()
+    let language x =
+        match x with
+        | "f#" | "prolog" -> "Пoдлизa!"
+        | "pascal" -> "Мое почтение, господин"
+        | _ -> "Отличный выбор!"
+//каррирование
+    Console.WriteLine("Какoй твoй любимый язык?")
+    (Console.ReadLine >> lower >> language >> Console.WriteLine)()
+
+//суперпозиция
+    Console.WriteLine("Какoй твoй любимый язык?")
+    let dop input (output:string->unit) chooser = output (chooser (input ()))
+    dop Console.ReadLine Console.WriteLine language
+
+    0
