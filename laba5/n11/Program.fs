9670d9804687960e9f77d48bfc66de0d363bf9c0
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-17 00:05:08 +0300

    Добавьте файлы проекта.

---
+++ laba5/n11/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let language = function
+    | "f#" | "prolog" -> "Пoдлизa!"
+    | "pascal" -> "Мое почтение, господин"
+    | _ -> "Отличный выбор!"
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Какoй твoй любимый язык?")
+    let s= Console.ReadLine().ToLower()
+    Console.WriteLine(language s)
+    0
