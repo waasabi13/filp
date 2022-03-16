a86492b604fcf1f70cfd876fb55f50911f8b0ba1
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-17 00:23:27 +0300

    Реализована рекурсия для нахождения произведения цифр числа

---
+++ laba5/n13/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+let rec pr_down x=
+    if x=0 then
+        1
+    else (x%10)*pr_down(x/10)
+let rec pr_up x=
+    let rec pr_up x pr=
+        if x = 0 then pr
+        else
+            let newpr= pr*(x%10)
+            let x1 = x/10
+            pr_up x1 newpr
+    pr_up x 1
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Произведение чисел числа x: ")
+    let x = Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine(pr_down x)
+    Console.WriteLine(pr_up x)
+    0 // return an integer exit code
