00536a5fe1cf4d0e7cc2e144c7bc88e667e15f2a
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-17 18:51:19 +0300

    14

---
+++ laba5/n14/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let f n predicate init =
+    let rec f_add x func init divider =
+        if divider = 0 then init
+        else
+            let newInit = 
+                if x % divider = 0 then 
+                    func init divider 
+                else init
+            let newDivider = divider - 1
+            f_add x func newInit newDivider
+    f_add n predicate init n
+    
+[<EntryPoint>]
+let main argv =
+    System.Console.WriteLine(f 12 (fun x y -> x + y) 0)
+    0
