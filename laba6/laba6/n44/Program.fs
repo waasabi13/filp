b95fb3c755049574eefcc7a12d1dc7d8d54e38da
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-21 21:03:12 +0300

    Сюда не смотрите

--- laba6/laba6/n44/Program.fs
+++ laba6/laba6/n44/Program.fs
@@ -8,21 +8,32 @@ let rec readlist1 n =
     let Head = Console.ReadLine()|>Convert.ToDouble
     let Tail = readlist1 (n-1)
     Head::Tail
-
-let check list check=
-    let rec c1 list b check=
+(*let check2 (list:float list)=
+    if list.Length=1 then false
+    else
+        let rec c1 list b=
+            match list with
+            | a::b::t-> 
+                if ((a%1.0=0.0 && b%1.0=0.0) ||(a%1.0<>0.0 && b%1.0<>0.0)) then false
+                else c1 t b
+            | a::t->
+                if ((a%1.0=0.0 && b%1.0=0.0) ||(a%1.0<>0.0 && b%1.0<>0.0)) then false
+                else true
+            | []-> true
+        c1 list 0.0
+*)
+let check (list:float list)=
+    let rec c1 list b=
         match list with
-        | a::b::t-> 
+        | a::t-> 
             if ((a%1.0=0.0 && b%1.0=0.0) ||(a%1.0<>0.0 && b%1.0<>0.0)) then false
-            else c1 t b check
-        | a::t->
-            if ((a%1.0=0.0 && b%1.0=0.0) ||(a%1.0<>0.0 && b%1.0<>0.0)) then false
-            else true
-        | []-> check
-    c1 list 0.0 true
+            else c1 t a
+        | []-> true
+    c1 list.Tail list.Head
+
 [<EntryPoint>]
 let main argv =
    let n = Console.ReadLine()|> Int32.Parse
    let list = readlist1 n
-   Console.WriteLine(check list true)
+   Console.WriteLine(check list)
    0 // return an integer exit code
