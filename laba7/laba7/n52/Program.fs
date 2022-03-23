986de56dda008ff1aaf2028e50fb5b1c67c83f32
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-23 23:23:27 +0300

    ))))

---
+++ laba7/laba7/n52/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let proc number =
+    let rec pr num del =
+        if num <= 1 then []
+        else 
+            let new_del = del+1
+            if num % del =0 then List.append[del] (pr (num/del) del)                        
+            else pr num new_del  
+    pr number 2 
+[<EntryPoint>]
+let main argv =
+    let number = System.Convert.ToInt32(System.Console.ReadLine())
+    Program.writelist(proc number)
+    0
