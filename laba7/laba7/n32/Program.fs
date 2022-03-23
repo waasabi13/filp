5e55fcdaf23dc364d178f9af34296c0469d261ff
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-23 22:28:49 +0300

    :3

---
+++ laba7/laba7/n32/Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+let proc (list:'int list)=List.fold (fun s x->if x then s+1 else s) 0 (List.map3 (fun x y z->(y>x)&&(y>z)) list.[0..list.Length-3] list.[1..list.Length-2] list.[2..list.Length-1]) 
+[<EntryPoint>]
+let main argv =
+    let list= Program.readlist 6
+    Console.WriteLine(proc list)
+    0 // return an integer exit code
