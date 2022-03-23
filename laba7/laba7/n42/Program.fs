96969a76cc9ff94a61800fc75a8d58208cd977a6
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-23 22:51:37 +0300

    сделаль

---
+++ laba7/laba7/n42/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+let rec readList1 n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToDouble(System.Console.ReadLine())
+    let Tail = readList1 (n-1)
+    Head::Tail
+let reslist list = List.filter (fun x->x<List.average list) list
+[<EntryPoint>]
+let main argv =
+    let list=readList1 5
+    Program.writelist(reslist list)
+    0 
