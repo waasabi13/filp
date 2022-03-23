69e65f41811b8115855dbcad0918b6518bf8c234
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-23 21:51:48 +0300

    Обожаю встроенный List

---
+++ laba7/laba7/n22/Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+let result list a b= List.fold (fun s x -> if (x=List.min list && x>a && x<b) then s+1 else s) 0 list
+[<EntryPoint>]
+let main argv =
+    let list= Program.readlist 5
+    Console.WriteLine(result list 1 3)
+    0
