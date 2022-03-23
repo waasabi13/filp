263e4b25b65e646bd7a2cc85f1121c52036ad733
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-23 21:35:15 +0300

    2 лаба

---
+++ laba7/laba7/n12/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+let find_min list=List.findIndex(fun x->x=List.min list) list
+let find_max list=List.findIndex(fun x->x=List.max list) list
+let result_list_reverse_between_index_of_min_and_max_element_special_for_arseniy_sergeevich_juk_from_kirill_titov_gruppa_26_1_fiit_fktipm_kubsu_plz_postavte_5 list= 
+    let start=Math.Min(find_max list,find_min list)
+    let endpoint=Math.Max(find_min list,find_max list)
+    let piece=list.[start+1..endpoint-1]
+    list.[0..start]@ (List.rev piece) @ list.[endpoint..list.Length-1]
+[<EntryPoint>]
+let main argv =
+    let list= Program.readlist 7
+    Program.writelist(result_list_reverse_between_index_of_min_and_max_element_special_for_arseniy_sergeevich_juk_from_kirill_titov_gruppa_26_1_fiit_fktipm_kubsu_plz_postavte_5 list)
+    0 
--- laba7/laba7/n2/Program.fs
+++ laba7/laba7/n2/Program.fs
@@ -2,7 +2,7 @@
 let rec readlist n =
     List.init(n) (fun index->Console.ReadLine()|>Int32.Parse)
 let rec writelist list=
-    List.iter(fun x->printf "%O" x) list
+    List.iter(fun x->printfn "%O" x) list
 let find_min list=List.findIndex(fun x->x=List.min list) list
 
 [<EntryPoint>]
