2aa220bafc9e3b82e803f47bbef1bfdd750464d6
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-23 21:16:50 +0300

    Добрый вечер, начал решать лабу 7

---
+++ laba7/laba7/n2/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+let rec readlist n =
+    List.init(n) (fun index->Console.ReadLine()|>Int32.Parse)
+let rec writelist list=
+    List.iter(fun x->printf "%O" x) list
+let find_min list=List.findIndex(fun x->x=List.min list) list
+
+[<EntryPoint>]
+let main argv =
+    let list= readlist 5
+    Console.WriteLine(find_min list)
+    0 // return an integer exit code
