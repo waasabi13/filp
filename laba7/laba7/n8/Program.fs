9e84964ca6ad78548a8a0c38e69996819d01344e
kiril <kiril@LAPTOP-T1KK7D8U>
2022-03-24 09:07:46 +0300

    сделал

---
+++ laba7/laba7/n8/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let array1=[|1;2;3|]
+    let array2=[|4;5;7|]
+    let result=Array.append array1 [|(Array.item(array2.Length-1) array2)|] 
+    printfn"%A"result
+    0 // return an integer exit code
