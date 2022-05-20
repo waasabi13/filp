01f4dc8236c121fe010548d509edd20c62ef4920
kiril <kiril@LAPTOP-T1KK7D8U>
2022-05-20 21:09:20 +0300

    9 zadanie + oshibki 8

--- laba9/laba9/lr9/Program.fs
+++ laba9/laba9/lr9/Program.fs
@@ -1,26 +1,26 @@
 ﻿open System
 open System.Windows.Forms
 open System.Drawing
-open System.IO
+
 
 let form = new Form(Width=750, Height = 500, Text = "F# main form", Menu = new MainMenu())
 
-let text = new Label(Font = new Font(familyName = "Segoe UI",emSize = 15.0f), Height = 30, Width= 550, Text="Введите массивы")
-let text1 = new Label(Font = new Font(familyName = "Segoe UI",emSize = 15.0f),Width = 200, Top = 50, Height = 70, Left = 0,Text="Массив А")
-let text2 = new Label(Font = new Font(familyName = "Segoe UI",emSize = 15.0f),Width = 200, Top = 120, Height = 70, Left = 0, Text="Массив Б")
-let text3 = new Label(Font = new Font(familyName = "Segoe UI",emSize = 15.0f),Width = 200, Top = 190, Height = 70, Left = 0, Text="Результирующий массив")
+let text = new Label(Font = new Font(familyName = "Segoe UI",emSize = 15.0f), Height = 30, Width= 550, Text="Введите список")
+let text1 = new Label(Font = new Font(familyName = "Segoe UI",emSize = 15.0f),Width = 200, Top = 50, Height = 70, Left = 0,Text="Список для сортировки")
+let text3 = new Label(Font = new Font(familyName = "Segoe UI",emSize = 15.0f),Width = 200, Top = 190, Height = 70, Left = 0, Text="Отсортированный список")
 let richTextBox2 = new RichTextBox(Font = new Font(familyName = "Segoe UI",emSize = 22.0f),Width = 500, Top = 50, Height = 70, Left = 200,Text="1,2,3")
-let richTextBox3 = new RichTextBox(Font = new Font(familyName = "Segoe UI",emSize = 22.0f),Width = 500, Top = 120,Height = 70, Left = 200,Text="4,5,7")
 let richTextBox4 = new RichTextBox(Font = new Font(familyName = "Segoe UI",emSize = 22.0f),Width = 500, Top = 190,Height = 70, Left = 200,Text="")
 let button1 = new Button(Height=50, Width = 150, Top = 0, Left = 550,Text="Вычислить")
 let _ = button1.Click.Add((fun (e:EventArgs) -> 
     try 
+            
         let a = (richTextBox2.Text).Split( [|','|])
-        let b = (richTextBox3.Text).Split( [|','|])
-        let c = Array.append a ([|b.[b.Length-1]|])
+        let c = Array.sort a
         let rec ArrayToString (arr:string []) i acc:string =
             if (i = arr.Length) then acc
-            else ArrayToString arr (i+1) (acc + "," + arr.[i])
+            else if (arr.[i] <> " " && arr.[i] <> "")
+            then ArrayToString arr (i+1) (acc + "," + arr.[i])
+            else ArrayToString arr (i+1) (acc)
         let output = (ArrayToString c 0 "")
         richTextBox4.Text <- output.[1..]
     with
@@ -29,11 +29,9 @@ let _ = button1.Click.Add((fun (e:EventArgs) ->
         ))
 form.Controls.Add(text)
 form.Controls.Add(text1)
-form.Controls.Add(text2)
 form.Controls.Add(text3)
 form.Controls.Add(richTextBox2)
-form.Controls.Add(richTextBox3)
 form.Controls.Add(richTextBox4)
 form.Controls.Add(button1)
 Application.Run(form)    
-0 
+0
